eb41be48845852b5732871e8a3785a2d18167115
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-05-01 22:29:45 +0530

    Create Pattern printing5.java

---
+++ Pattern printing5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+
+/* output:
+5
+12345
+12345
+12345
+12345
+12345
+*/
