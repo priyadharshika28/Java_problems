90eb4f9dbe2e951ba8640fa555f1dec38b63c281
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-05-01 15:42:12 +0530

    Create Polybags.java

---
+++ Polybags.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for(int i=0;i<n;i++){
+		    int a = sc.nextInt();
+		    int bags = (a+9)/10;
+		    System.out.println(bags);
+		}
+
+	}
+}
