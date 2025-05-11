75a721e55fd347629c992bac5253f6e9eee4addc
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-05-11 11:22:44 +0530

    Create Movegrid_cf.java

---
+++ Movegrid_cf.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int cor1 = a-c;
+		int cor2 = b-d;
+		System.out.println(cor1+" "+cor2);
+	}
+}
