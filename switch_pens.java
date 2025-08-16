9ffa9f8601738d5a2dc2f91ff63d2635d587f437
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-08-16 16:36:30 +0530

    Create switch_pens.java

---
+++ switch_pens.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]%2!=0){
+                if(arr[i+1]%2==0){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
