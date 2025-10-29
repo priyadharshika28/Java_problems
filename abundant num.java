fe9e29a8cad97fb0064a64507b11988ca89b4e33
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2025-10-29 21:06:07 +0530

    Create abundant num.java

---
+++ abundant num.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0;
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+        if(sum>n){
+            System.out.println("abundant num");
+        }
+        else{
+            System.out.println("not abundant num");
+        }
+    }
+}
